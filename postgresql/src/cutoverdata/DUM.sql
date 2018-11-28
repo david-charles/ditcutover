a8fa878308d286de1e87b2324e91648396b9b1ff
David Charles <david.charles@maplin.co.uk>
2018-11-28 11:15:40 +0000

    Initial creation

---
+++ postgresql/src/cutoverdata/DUM.sql
@@ -0,0 +1,153 @@
+-- clone parameters:
+-- p_src_regulation_id,
+-- p_src_measure_type, 
+-- p_regulation_role, 
+-- p_regulation_id, 
+-- p_validity_start_date, 
+-- p_measure_type,
+-- p_regulation_group_id, 
+-- p_information_text
+
+
+-- PASTED FROM EXCEL SHEET...23/11/18
+
+delete from sp_stats;
+SELECT cleanup_changes_since('2018.11.22');
+SELECT end_date('I1404330','565',1,'Z1900010','30.03.2019');
+select * from sp_stats;
+select sum(rowcount) from sp_stats;
+
+SELECT end_date('I1404330','565',1,'Z1900010','30.03.2019');
+SELECT end_date('I1404570','565',1,'Z1900010','30.03.2019');
+SELECT end_date('I1702640','561',1,'Z1900010','30.03.2019');
+SELECT end_date('I1702900','565',1,'Z1900010','30.03.2019');
+SELECT end_date('I1703770','565',1,'Z1900010','30.03.2019');
+SELECT end_date('I1704160','565',1,'Z1900010','30.03.2019');
+SELECT end_date('J1703340','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1723000','564',1,'Z1900010','30.03.2019');
+SELECT end_date('R0908030','464',1,'Z1900010','30.03.2019');
+SELECT end_date('R0908030','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1113310','464',1,'Z1900010','30.03.2019');
+SELECT end_date('R1113310','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1200140','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1205850','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1207960','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1210390','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1300780','464',1,'Z1900010','30.03.2019');
+SELECT end_date('R1300780','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1304300','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1305080','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1308610','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1308710','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1308750','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1313430','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1401910','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1403600','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1404710','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1409760','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1412830','464',1,'Z1900010','30.03.2019');
+SELECT end_date('R1412830','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1413130','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1413460','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1500820','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1500830','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1500840','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1514290','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1519340','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1519520','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1519630','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1522720','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1522721','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1523840','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1523850','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1600320','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1603870','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1603880','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1607030','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1610460','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1610770','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1611590','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1611600','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1612470','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1700940','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1702710','464',1,'Z1900010','30.03.2019');
+SELECT end_date('R1704210','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1704220','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1706480','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1707630','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1708040','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1709420','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1710190','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1711710','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1711870','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1711880','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1717590','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1719930','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1722060','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1722300','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1207950','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1212690','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1303720','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1403070','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1510192','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1513940','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1518212','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1603060','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1607040','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1702200','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1706590','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1711460','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1711590','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1714820','554',1,'Z1900010','30.03.2019');
+SELECT end_date('R1715780','552',1,'Z1900010','30.03.2019');
+SELECT end_date('R1722130','552',1,'Z1900010','30.03.2019');
+
+
+
+select count(*) from measures_oplog where justification_regulation_id='Z1900010';
+select count(*) from measures_oplog where operation_date=current_date;
+select count(*) from base_regulations_oplog where operation_date=current_date;
+select * from base_regulations_oplog where operation_date=current_date;
+select * from base_regulations_oplog where base_regulation_id like 'Z%';
+select count(distinct measure_generating_regulation_id) from measures_oplog where operation_date=current_date and measure_generating_regulation_id like 'N%';
+
+
+
+SELECT clone('R1606700','465',1,'N9909990','30.03.2019','465','MLA','UK continuation for R1606700 : Introducing prior Union surveillance of imports of certain iron and steel products originating in certain third countries');
+SELECT clone('R1701091','110',1,'N1900090','30.03.2019','110','UTS','UK continuation for R1701091 : DUMP (552) - CN - chap 87 - Supplementary units');
+SELECT clone('R1211530','552',1,'N1900030','30.03.2019','552','DUM','UK continuation for R1211530 : DUMP (552) - CN - Chap 41');
+SELECT clone('R1301570','464',1,'N1900110','30.03.2019','464','DUM','UK continuation for R1301570 : DUMP (552) - US - Chap 22, 27, 38');
+SELECT clone('R1301570','552',1,'N1900110','30.03.2019','552','DUM','UK continuation for R1301570 : DUMP (552) - US - Chap 22, 27, 38');
+SELECT clone('R1302140','552',1,'N1900130','30.03.2019','552','DUM','UK continuation for R1302140 : DUMP (552) - CN - Chap. 72');
+SELECT clone('R1302150','554',1,'N1900290','30.03.2019','554','DUM','UK continuation for R1302150 : COMP (554) - CN - Chap. 72');
+SELECT clone('R1302170','552',1,'N1900120','30.03.2019','552','DUM','UK continuation for R1302170 : DUMP (552) - CN - Chap. 76');
+SELECT clone('R1304120','552',1,'N1900140','30.03.2019','552','DUM','UK continuation for R1304120 : DUMP (552) - CN - Chap. 69');
+SELECT clone('R1304610','554',1,'N1900250','30.03.2019','554','DUM','UK continuation for R1304610 : COMP (554) - IN - Chap. 39');
+SELECT clone('R1306950','552',1,'N1900040','30.03.2019','552','DUM','UK continuation for R1306950 : DUMP (552) - CN - Chap. 39, 44,73, 85');
+SELECT clone('R1409990','552',1,'N1900010','30.03.2019','552','DUM','UK continuation for R1409990 : DUMP (552) - RU - Chap. 31');
+SELECT clone('R1413790','554',1,'N1900270','30.03.2019','554','DUM','UK continuation for R1413790 : COMP (554)    -   CN     -     Chap 70');
+SELECT clone('R1501100','552',1,'N1900050','30.03.2019','552','DUM','UK continuation for R1501100 : DUMP (552) - BY, CN, RU and Termination (552) - UA - Chap. 73');
+SELECT clone('R1503090','554',1,'N1900300','30.03.2019','554','DUM','UK continuation for R1503090 : COMP (554) - TR - Chap. 3');
+SELECT clone('R1508650','552',1,'N1900060','30.03.2019','552','DUM','UK continuation for R1508650 : DUMP (552) - CN, Chap 72, 73');
+SELECT clone('R1515180','552',1,'N1900080','30.03.2019','552','DUM','UK continuation for R1515180 : DUMP (552) - US, CA - Chap 15, 27, 38 (Biodiesel)');
+SELECT clone('R1515190','554',1,'N1900260','30.03.2019','554','DUM','UK continuation for R1515190 : COMP (554) - US, CA - Chap 15, 27, 38 (Biodiesel)');
+SELECT clone('R1518460','552',1,'N1900070','30.03.2019','552','DUM','UK continuation for R1518460 : DUMP (552) - CN - Chap. 72');
+SELECT clone('R1519530','552',1,'N1900160','30.03.2019','552','DUM','UK continuation for R1519530 : DUMP (552) - CN, JP, KR, RU, US - Chap. 72');
+SELECT clone('R1612460','552',1,'N1900170','30.03.2019','552','DUM','UK continuation for R1612460 : DUMP (552) - CN - Chap. 72');
+SELECT clone('R1613280','552',1,'N1900180','30.03.2019','552','DUM','UK continuation for R1613280 : DUMP (552) - CN, RU - Chap. 72');
+SELECT clone('R1701090','552',1,'N1900090','30.03.2019','552','DUM','UK continuation for R1701090 : DUMP (552) - CN - chap 87');
+SELECT clone('R1703360','552',1,'N1900200','30.03.2019','552','DUM','UK continuation for R1703360 : DUMP (552) - CN - Chap. 72');
+SELECT clone('R1707240','552',1,'N1900150','30.03.2019','552','DUM','UK continuation for R1707240 : DUMP (552) - CN - Chap. 70');
+SELECT clone('R1709691','554',1,'N1900310','30.03.2019','554','DUM','UK continuation for R1709691 : COMP (554) - CN - Chap. 72');
+SELECT clone('R1711410','554',1,'N1900280','30.03.2019','554','DUM','UK continuation for R1711410 : COMP (554) - IN - Chap 72');
+SELECT clone('R1717950','552',1,'N1900210','30.03.2019','552','DUM','UK continuation for R1717950 : DUMP (552) - BR, IR, UA, RU - Chap. 72');
+SELECT clone('R1721790','552',1,'N1900100','30.03.2019','552','DUM','UK continuation for R1721790 : DUMP (552) - CN - Chap. 69');
+SELECT clone('R1401900','554',1,'N1900250','30.03.2019','554','DUM','UK continuation for R1401900 : COMP (554) - IN - Chap. 39');
+SELECT clone('R1408030','552',1,'N1900140','30.03.2019','552','DUM','UK continuation for R1408030 : DUMP (552) - CN (B956,B957,B958,B959) - Chap. 69');
+SELECT clone('R1513500','554',1,'N1900250','30.03.2019','554','DUM','UK continuation for R1513500 : COMP (554) - IN - Chap. 39');
+SELECT clone('R1602260','552',1,'N1900010','30.03.2019','552','DUM','UK continuation for R1602260 : DUMP (552) - RU (A959) - Chap. 31');
+SELECT clone('R1606750','554',1,'N1900260','30.03.2019','554','DUM','UK continuation for R1606750 : DUMP (554) - CA - Chap. 15,27,38');
+SELECT clone('R1606760','552',1,'N1900080','30.03.2019','552','DUM','UK continuation for R1606760 : DUMP (552) - CA- Chap. 15,27,38');
+SELECT clone('R1709692','552',1,'N1900310','30.03.2019','552','DUM','UK continuation for R1709692 : DUMP (552) - CN - Chap. 72');
+SELECT clone('R1710920','465',1,'N9909990','30.03.2019','465','MLA','UK continuation for R1710920 : Amending Reg 670/2016 introducing prior Union surveillance of imports of certain iron and steel products originating in certain third countries');
+SELECT clone('R1722070','552',1,'N1900140','30.03.2019','552','DUM','UK continuation for R1722070 : DUMP (552) - CN - Chap 69');
